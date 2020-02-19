6e35f516c5199bf1e9becede58d5a15ee5d35d68
LFDB <philippe@lafabriquedeblogs.com>
2020-02-19 07:28:26 -0500

    css form hubspot

--- js/selector.js
+++ js/selector.js
@@ -5,6 +5,11 @@
 			
 			$(this).toggleClass("selected");
 		});
+		$('#gform_wrapper_3').on('click','#field_3_4 label[id*="label_3_4_"], #field_3_9 label[id*="label_3_9_"], #field_3_21 label[id*="label_3_21_"],#field_3_23 label[id*="label_3_23_"]', function(e){
+			e.preventDefault();
+			$(this).toggleClass("selected");
+		});
+
 	});
 	
 } )(jQuery, document);
